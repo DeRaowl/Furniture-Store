46256db2b0af853bf3c020e53d5166f90b495de4
Rahul M <rahulreddy93m@gmail.com>
2021-09-15 17:46:34 +0530

    feat: implemented router

---
+++ src/pages/index.js
@@ -0,0 +1,19 @@
+import Home from "./HomePage";
+import About from "./AboutPage";
+import Cart from "./CartPage";
+import Checkout from "./CheckoutPage";
+import Products from "./ProductsPage";
+import SingleProducts from "./SingleProductPage";
+import PrivateRoute from "./PrivateRoute";
+import Error from "./ErrorPage";
+
+export {
+  Home,
+  About,
+  Cart,
+  Checkout,
+  Products,
+  SingleProducts,
+  PrivateRoute,
+  Error,
+};
