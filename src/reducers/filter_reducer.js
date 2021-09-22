802780590314457b15b209ad4a6942da09a22058
Rahul M <rahulreddy93m@gmail.com>
2021-09-22 13:48:50 +0530

    feat: added sort function

--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -18,7 +18,56 @@ const filter_reducer = (state, action) => {
         filtered_products: [...action.payload],
       };
       break;
+    case SET_GRIDVIEW:
+      return {
+        ...state,
+        grid_view: true,
+      };
+      break;
+    case SET_LISTVIEW:
+      return {
+        ...state,
+        grid_view: false,
+      };
+      break;
+
+    case UPDATE_SORT:
+      return {
+        ...state,
+        sort: action.payload,
+      };
+      break;
+    case SORT_PRODUCTS:
+      const { sort, filtered_products } = state;
+      let tempProducts = [...filtered_products];
+      if (sort === "price-lowest") {
+        tempProducts = tempProducts.sort((a, b) => {
+          if (a.price < b.price) {
+            return -1;
+          }
+          if (a.price > b.price) {
+            return 1;
+          }
+          return 0;
+        });
+      }
+      if (sort === "price-highest") {
+        tempProducts = tempProducts.sort((a, b) => b.price - a.price);
+      }
+      if (sort === "name-a") {
+        tempProducts = tempProducts.sort((a, b) => {
+          return a.name.localeCompare(b.name);
+        });
+      }
+      if (sort === "name-z") {
+        tempProducts = tempProducts.sort((a, b) => {
+          return b.name.localeCompare(a.name);
+        });
+      }
+      return { ...state, filtered_products: tempProducts };
+      break;
   }
+
   throw new Error(`No Matching "${action.type}" - action type`);
 };
 
