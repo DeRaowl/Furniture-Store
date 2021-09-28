894e3963f1da30f151c20a4ea333096009f9b4a9
Rahul M <rahulreddy93m@gmail.com>
2021-09-28 20:46:36 +0530

    feat: added Authentication

--- src/index.js
+++ src/index.js
@@ -9,10 +9,21 @@ import { UserProvider } from "./context/user_context";
 import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.render(
-  <ProductsProvider>
-    <FilterProvider>
-      <App />
-    </FilterProvider>
-  </ProductsProvider>,
+  <Auth0Provider
+    domain="deraowl.us.auth0.com"
+    clientId="sxZo1Xz01G6JgN2neImTDBDZCu0VQ40T"
+    redirectUri={window.location.origin}
+    cacheLocation="localstorage"
+  >
+    <UserProvider>
+      <ProductsProvider>
+        <FilterProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </FilterProvider>
+      </ProductsProvider>
+    </UserProvider>
+  </Auth0Provider>,
   document.getElementById("root")
 );
